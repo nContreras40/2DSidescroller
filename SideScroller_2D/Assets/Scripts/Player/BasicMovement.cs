64b3216e66c0e30b121da50784b529a52cc6f364
Nathan Contreras <nContreras40@users.noreply.github.com>
2017-06-28 00:36:01 -0500

    NRC:  Re-adding files, now re-organized. - Added basic script allowing jumping and movement. - Added custom launcher icons, splash screen, and cursor. - Updated a few graphics.

---
+++ SideScroller_2D/Assets/Scripts/Player/BasicMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Component Rigidbody2D required for script to be functional.
+[RequireComponent(typeof(Rigidbody2D))]
+
+//=============================================================================
+// BasicMovement class is responsible for providing base-line functional
+// movement to the player character.
+public class BasicMovement : MonoBehaviour {
+
+    [Tooltip("The layer which will hold collidable floor environments")]
+    public LayerMask groundLayer;
+
+    [Tooltip("The speed of the player.")]
+    public float playerSpeed = 5.0f;
+    [Tooltip("The jump force of the player.")]
+    public float jumpForce = 5.0f;
+
+    [Tooltip("The distance we want to check when raycasting to find the ground.")]
+    public float lengthCheck = 0.55f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // Update is called once per frame
+    void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = 0.0f;
+
+        Debug.DrawLine(this.transform.position, new Vector2(this.transform.position.x, this.transform.position.y - 0.55f));
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, Vector2.down, 0.55f, groundLayer);
+
+        if (hit)
+        {
+            v = Input.GetAxis("Vertical");
+        }
+
+        Vector2 movement = new Vector2(h * playerSpeed, v * jumpForce);
+        this.GetComponent<Rigidbody2D>().velocity = movement;
+	}
+}
---
+++ SideScroller_2D/Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int startingHealth = 3;
+    public int healthPerHeart = 3;
+    public int currentHealth = 0;
+
+    public GameObject[] allHearts;
+
+    private int currentHeartIndex = 0;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        currentHealth = startingHealth;
+        addContainers(this.startingHealth);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            addContainers(1);
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            removeContainers(1);
+        }
+    }
+
+    // Adds a defined amount of heart containers
+    public void addContainers(int amount)
+    {
+        if (currentHeartIndex >= 9)
+            return;
+
+        for (int ix = 0; ix < amount; ix++)
+        {
+            allHearts[currentHeartIndex].SetActive(true);
+            currentHeartIndex++;
+        }
+
+        foreach(GameObject go in allHearts)
+        {
+            go.GetComponent<Animator>().Play("FullHeart", -1, 0f);
+        }
+
+        Debug.Log(currentHeartIndex);
+    }
+
+    // removes a defined amount of heart containers
+    public void removeContainers(int amount)
+    {
+        if (currentHeartIndex <= 0)
+            return;
+
+        for (int ix = 0; ix < amount; ix++)
+        {
+            currentHeartIndex--;
+            allHearts[currentHeartIndex].SetActive(false);
+        }
+
+        Debug.Log(currentHeartIndex);
+    }
+
+    public void modifyHealth(int amount)
+    {
+        for (int ix = 0; ix < amount; ix++)
+        {
+        }
+    }
+}
+
